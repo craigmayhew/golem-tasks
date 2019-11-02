0134c68ff1a92ed8b30a390e10cf1c815ff0476d
Craig Mayhew <craig@mayhew.io>
2019-11-02 19:26:32 +0000

    Now using md5 work as a test

--- golem/gwasm/prime/src/main.rs
+++ golem/gwasm/prime/src/main.rs
@@ -1,18 +1,36 @@
-use std::env;
 use std::fs;
-use std::io::{self, Read, Write};
+use std::io::{self, Write};
+
+extern crate md5;
+extern crate rand;
+
+use rand::Rng;
+use rand::distributions::Alphanumeric;
 
 fn main() -> io::Result<()> {
-    let args = env::args().collect::<Vec<String>>();
-    let name = args.get(1).map_or("anonymous".to_owned(), |x| x.clone());
 
-    let mut in_file = fs::File::open("in.txt")?;
-    let mut contents = String::new();
-    in_file.read_to_string(&mut contents)?;
+    /*let mut in_file = fs::File::open("in.txt")?;
+    let mut input = String::new();
+    in_file.read_to_string(&mut input)?;*/
 
-    let mut out_file = fs::File::create("out.txt")?;
-    out_file.write_all(&contents.as_bytes())?;
-    out_file.write_all(&name.as_bytes())?;
+    let difficulty:usize = 5;
 
+    let mut hash = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .collect::<String>();
+
+    for _n in 0..1048576 {
+        let input_hash:String = hash;
+        let mut attempt = String::from("My name is ZeroPointCraig and this file has a hash of ");
+        attempt.push_str(&input_hash);
+        hash = format!("{:x}", md5::compute(&attempt));
+        if hash[0..difficulty] == input_hash[0..difficulty] {
+            //only write out.txt if we find an answer!
+            let mut out_file = fs::File::create("out.txt")?;
+            out_file.write_all(&attempt.as_bytes())?;
+        }
+    }
+    
     Ok(())
 }
