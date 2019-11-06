a32a713f4fa9059d0c722ceed3527093d6a912d9
Craig Mayhew <craig@mayhew.io>
2019-11-06 21:04:05 +0000

    Added hello world lzma code and crate

--- gwasm/compression/src/main.rs
+++ gwasm/compression/src/main.rs
@@ -1,7 +1,17 @@
 use std::io::{self, Write};
 
-fn main() -> io::Result<()> {
-    
+extern crate lzma;
 
+use lzma::LzmaWriter;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let f = File::create("out.xz").unwrap();
+	let mut f = LzmaWriter::new_compressor(f, 6).unwrap();
+
+	write!(f, "It's a small world!").unwrap();
+	f.finish().unwrap();
+    
     Ok(())
 }
