8409d435528a3b78652c91dd2265a032248eabd1
Craig Mayhew <craig@mayhew.io>
2019-11-09 22:43:11 +0000

    Refactor to use new fn is_prime_check()

--- gwasm/prime-check/src/main.rs
+++ gwasm/prime-check/src/main.rs
@@ -19,19 +19,12 @@ fn is_prime_check(possible_prime:String) -> String {
 fn main() -> io::Result<()> {
     //setup out.txt file
     let mut out_file = fs::File::create("out.txt")?;
-    let mut output:String = String::from(" ");
 
     //get first argument
     let args = env::args().collect::<Vec<String>>();
     let possible_prime = args.get(1).map_or("no value found".to_owned(), |x| x.clone());
     
-    //prime check
-    output.push_str(&possible_prime);
-    if prime_tools::is_u64_prime(possible_prime.parse::<u64>().unwrap()) {
-        output.push_str(" is prime!");
-    } else {
-        output.push_str(" is not prime :(");
-    }
+    let output:String = is_prime_check(possible_prime);
 
     //wipe contents and write out entire file
     out_file.write_all(output.as_bytes())?;
