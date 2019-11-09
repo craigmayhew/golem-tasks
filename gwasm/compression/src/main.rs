eaed0a35d2eacb1b84c42d0cfaeacad8e93e1327
Craig Mayhew <craig@mayhew.io>
2019-11-09 20:38:44 +0000

    Output filename is now simply input file name with a .7z suffix

--- gwasm/compression/src/main.rs
+++ gwasm/compression/src/main.rs
@@ -11,14 +11,14 @@ fn main() -> io::Result<()> {
 	let args = env::args().collect::<Vec<String>>();
 	let filename = args.get(1).map_or("no value found".to_owned(), |x| x.clone());
 	
-	let mut file = std::io::BufReader::new(std::fs::File::open(filename).unwrap());
+	let mut file = std::io::BufReader::new(std::fs::File::open(&filename).unwrap());
 
-	// "comp" can be anything that implements "std::io::Write"
+	let filename_7z = format!("{}.7z",&filename);
 	let mut compressed: Vec<u8> = Vec::new();
 	lzma_compress(&mut file, &mut compressed).unwrap();
 
 	//write compressed content to disk
-	let mut out_file = fs::File::create("out.7z")?;
+	let mut out_file = fs::File::create(filename_7z)?;
 	out_file.write_all(&compressed)?;
 
     Ok(())
