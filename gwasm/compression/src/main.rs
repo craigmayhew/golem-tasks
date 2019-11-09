060a42ed16df618007020881ba639cee75292fd1
Craig Mayhew <craig@mayhew.io>
2019-11-09 23:06:12 +0000

    Refacvtor to use write_file_to_disk()

--- gwasm/compression/src/main.rs
+++ gwasm/compression/src/main.rs
@@ -16,17 +16,16 @@ fn write_file_to_disk(filename: String , data: Vec<u8>) -> io::Result<()> {
 
 fn main() -> io::Result<()> {
 	let args = env::args().collect::<Vec<String>>();
-	let filename = args.get(1).map_or("no value found".to_owned(), |x| x.clone());
+	let filename: String = args.get(1).map_or("no value found".to_owned(), |x| x.clone());
 	
 	let mut file = std::io::BufReader::new(std::fs::File::open(&filename).unwrap());
 
-	let filename_7z = format!("{}.7z",&filename);
 	let mut compressed: Vec<u8> = Vec::new();
 	lzma2_compress(&mut file, &mut compressed).unwrap();
 
 	//write compressed content to disk
-	let mut out_file = fs::File::create(filename_7z)?;
-	out_file.write_all(&compressed)?;
+	let filename_7z:String = format!("{}.7z",&filename);
+	write_file_to_disk(filename_7z, compressed).unwrap();
 
     Ok(())
 }
